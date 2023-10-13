14de7b33d2e91b0f979ea9c090a978c02cb4dc7c
Vardhman <113521400+Vardhman2712@users.noreply.github.com>
2023-10-13 23:10:53 +0530

    Create Min Cost Climbing Stairs.java
    Here's an explanation of the given code in bullet points:
    
    - **Objective:**
      - The code aims to find the minimum cost to climb a set of stairs represented by the array `cost`, where each step has a cost associated with it.
    
    - **Dynamic Programming Approach:**
      - It uses a dynamic programming approach to efficiently calculate the minimum cost to reach each step.
    
    - **Array Initialization:**
      - It creates an array `dp` of size `n` to store the minimum cost to reach each step, where `n` is the length of the `cost` array.
    
    - **Base Cases:**
      - Sets up base cases for the first and second steps:
        - `dp[0] = cost[0]` (minimum cost to reach the first step is the cost of the first step).
        - `dp[1] = cost[1]` (minimum cost to reach the second step is the cost of the second step).
    
    - **Iterative Calculation:**
      - Iterates through the array starting from the third step (index 2) up to the last step.
      - For each step `i`, calculates the minimum cost to reach that step by adding the cost of the current step (`cost[i]`) to the minimum cost of reaching the previous two steps (`Math.min(dp[i - 1], dp[i - 2])`).
    
    - **Final Result:**
      - The minimum cost to reach the top is the minimum of the last two steps, which is returned as `Math.min(dp[n - 1], dp[n - 2])`.
    
    - **Edge Case Handling:**
      - Checks if the length of the `cost` array is 0. If so, returns 0 (as there are no steps to climb).
    
    - **Time Complexity:**
      - The time complexity is O(n) since it iterates through the array once.
    
    - **Space Complexity:**
      - The space complexity is O(n) due to the additional array `dp` of the same size as the input array.

---
+++ Daily Problems/Min Cost Climbing Stairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        // Create an array to store the minimum cost to reach each step.
+        int[] dp = new int[n];
+
+        // Base case: the minimum cost to reach the first and second steps.
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        // Iterate through the remaining steps and calculate the minimum cost.
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        }
+
+        // The minimum cost to reach the top is the minimum of the last two steps.
+        return Math.min(dp[n - 1], dp[n - 2]);
+    }
+}
